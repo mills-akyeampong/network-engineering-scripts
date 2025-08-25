eb945627c66e223fcb557b5469189c832dbcb42a
mills-akyeampong <millsakyeampong@gmail.com>
2025-08-25 06:35:34 -0400

    ssh-to-devices updated

--- ssh/ssh-to-devices.py
+++ ssh/ssh-to-devices.py
@@ -11,3 +11,17 @@ for ip in devices:
     stdin, stdout, stderr = ssh.exec_command('show version')
     print(f"{ip}:\n{stdout.read().decode()}")
     ssh.close()
+    
+    
+    
+    #🔐 SSH to Multiple Devices (Paramiko)
+    
+    #📜 Script Summary
+    #Uses SSH connect to multiple devices and run commands like show version.
+    
+    #🔍 Explanation
+    #paramiko: Python SSH client
+    #exec_command(): Executes a command on the remote devicec.
+    #stdout.read().decode(): Reads the results and decodes bytes to text.
+    
+    #This is used for inventory collection, verification, or status checks across debices.
